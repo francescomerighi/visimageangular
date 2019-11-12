b2e101863916f2395eca26600cb9d7011ff7d72c
francescomerighi <francesco.merighi@marconirovereto.it>
2019-11-12 13:33:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-xvry6r
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-xvry6r)
---
+++ src/app/visimage/visimage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-visimage',
+  templateUrl: './visimage.component.html',
+  styleUrls: ['./visimage.component.css']
+})
+export class VisimageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
